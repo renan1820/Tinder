b234e800d5c9dae30e0a23a8920d39c1d955426e
Renan Castro <renancas_ribeiro@hotmail.com>
2020-10-18 02:14:53 -0300

    put rotate  animation on view

--- Tinder/Controller/CombineVC.swift
+++ Tinder/Controller/CombineVC.swift
@@ -13,20 +13,30 @@ class CombineVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.systemGroupedBackground
         
-        let redView = UIView()
-        redView.backgroundColor = .red
-        redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
-        
-        redView.center = view.center
-        
-        let gesture = UIPanGestureRecognizer()
-        gesture.addTarget(self, action: #selector(handleCard))
-        
-        redView.addGestureRecognizer(gesture)
-        
-        view.addSubview(redView)
+        self.adcionarCards()
     }
     
+}
+
+extension CombineVC{
+    func adcionarCards(){
+        
+        for item in 1...3 {
+
+            let redView = UIView()
+            redView.backgroundColor = item == 2 ? .blue : .red
+            redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+            
+            redView.center = view.center
+            
+            let gesture = UIPanGestureRecognizer()
+            gesture.addTarget(self, action: #selector(handleCard))
+            
+            redView.addGestureRecognizer(gesture)
+            
+            view.addSubview(redView)
+        }
+    }
 }
 
 extension CombineVC{
@@ -37,9 +47,14 @@ extension CombineVC{
             
             card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
             
+            let rotationAngle = point.x / view.bounds.width * 0.4
+            
+            card.transform = CGAffineTransform(rotationAngle: rotationAngle)
+            
             if gesture.state == .ended{
                 UIView.animate(withDuration: 0.2) {
                     card.center = self.view.center
+                    card.transform = .identity
                 }
             }
         }
