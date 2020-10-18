12af5c2ecb32337850da7a928c69fc266c50043b
Renan Castro <renancas_ribeiro@hotmail.com>
2020-10-18 01:37:12 -0300

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Renan de Castro Ribeiro on 18/10/20.
+//  Copyright © 2020 Renan de Castro Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
